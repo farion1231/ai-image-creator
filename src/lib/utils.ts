c0f9de281f91add2be8febc144698b5a6e8a0347
farion1231 <farion1231@gmail.com>
2025-06-06 22:40:13 +0800

    加了点注释

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// 智能合并类名，使用 clsx 和 tailwind-merge 合并类名
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
